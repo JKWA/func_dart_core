3207f5e9cfbadc6336c8f5b1149947714b86bb77
Joseph Koski <joe.koski@gmail.com>
2023-08-06 13:04:29 -0700

    Add pipe and flow

--- lib/function.dart
+++ lib/function.dart
@@ -38,3 +38,153 @@ class _FunctionSemigroup<S, A> extends BaseSemigroup<FunctionWrapper<S, A>> {
 
 /// Identity function.
 A identity<A>(A a) => a;
+
+/// Returns the input [a] without making any transformations.
+///
+/// Dart doesn't support function overloading, so different function names are
+/// used for a different number of function arguments.
+A pipe<A>(A a) {
+  return a;
+}
+
+/// Transforms the input [a] by applying the function [ab].
+B pipe2<A, B>(A a, B Function(A) ab) {
+  return ab(a);
+}
+
+/// Transforms the input [a] by applying the function [ab] and then [bc].
+C pipe3<A, B, C>(A a, B Function(A) ab, C Function(B) bc) {
+  return bc(ab(a));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc] and then [cd].
+D pipe4<A, B, C, D>(A a, B Function(A) ab, C Function(B) bc, D Function(C) cd) {
+  return cd(bc(ab(a)));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc], then [cd] and then [de].
+E pipe5<A, B, C, D, E>(A a, B Function(A) ab, C Function(B) bc,
+    D Function(C) cd, E Function(D) de) {
+  return de(cd(bc(ab(a))));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc], then [cd], then [de] and then [ef].
+F pipe6<A, B, C, D, E, F>(A a, B Function(A) ab, C Function(B) bc,
+    D Function(C) cd, E Function(D) de, F Function(E) ef) {
+  return ef(de(cd(bc(ab(a)))));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc], then [cd], then [de], then [ef] and then [fg].
+G pipe7<A, B, C, D, E, F, G>(A a, B Function(A) ab, C Function(B) bc,
+    D Function(C) cd, E Function(D) de, F Function(E) ef, G Function(F) fg) {
+  return fg(ef(de(cd(bc(ab(a))))));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc], then [cd], then [de], then [ef], then [fg] and then [gh].
+H pipe8<A, B, C, D, E, F, G, H>(
+    A a,
+    B Function(A) ab,
+    C Function(B) bc,
+    D Function(C) cd,
+    E Function(D) de,
+    F Function(E) ef,
+    G Function(F) fg,
+    H Function(G) gh) {
+  return gh(fg(ef(de(cd(bc(ab(a)))))));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc], then [cd], then [de], then [ef], then [fg], then [gh] and then [hi].
+I pipe9<A, B, C, D, E, F, G, H, I>(
+    A a,
+    B Function(A) ab,
+    C Function(B) bc,
+    D Function(C) cd,
+    E Function(D) de,
+    F Function(E) ef,
+    G Function(F) fg,
+    H Function(G) gh,
+    I Function(H) hi) {
+  return hi(gh(fg(ef(de(cd(bc(ab(a))))))));
+}
+
+/// Transforms the input [a] by applying the function [ab], then [bc], then [cd], then [de], then [ef], then [fg], then [gh], then [hi] and then [ij].
+J pipe10<A, B, C, D, E, F, G, H, I, J>(
+    A a,
+    B Function(A) ab,
+    C Function(B) bc,
+    D Function(C) cd,
+    E Function(D) de,
+    F Function(E) ef,
+    G Function(F) fg,
+    H Function(G) gh,
+    I Function(H) hi,
+    J Function(I) ij) {
+  return ij(hi(gh(fg(ef(de(cd(bc(ab(a)))))))));
+}
+
+/// Returns a function that applies [ab] to its argument.
+///
+/// This function is similar to `pipe2`, but it curries the first argument.
+/// Instead of taking two arguments, it takes one argument and returns a function
+/// that takes the second argument and applies [ab] to it.
+///
+/// Dart doesn't support function overloading, so different function names are
+/// used for a different number of function arguments.
+B Function(A) flow<A, B>(B Function(A) ab) => (A a) => pipe2(a, ab);
+
+C Function(A) flow2<A, B, C>(B Function(A) ab, C Function(B) bc) =>
+    (A a) => pipe3(a, ab, bc);
+
+D Function(A) flow3<A, B, C, D>(
+        B Function(A) ab, C Function(B) bc, D Function(C) cd) =>
+    (A a) => pipe4(a, ab, bc, cd);
+
+E Function(A) flow4<A, B, C, D, E>(B Function(A) ab, C Function(B) bc,
+        D Function(C) cd, E Function(D) de) =>
+    (A a) => pipe5(a, ab, bc, cd, de);
+
+F Function(A) flow5<A, B, C, D, E, F>(B Function(A) ab, C Function(B) bc,
+        D Function(C) cd, E Function(D) de, F Function(E) ef) =>
+    (A a) => pipe6(a, ab, bc, cd, de, ef);
+
+G Function(A) flow6<A, B, C, D, E, F, G>(
+        B Function(A) ab,
+        C Function(B) bc,
+        D Function(C) cd,
+        E Function(D) de,
+        F Function(E) ef,
+        G Function(F) fg) =>
+    (A a) => pipe7(a, ab, bc, cd, de, ef, fg);
+
+H Function(A) flow7<A, B, C, D, E, F, G, H>(
+        B Function(A) ab,
+        C Function(B) bc,
+        D Function(C) cd,
+        E Function(D) de,
+        F Function(E) ef,
+        G Function(F) fg,
+        H Function(G) gh) =>
+    (A a) => pipe8(a, ab, bc, cd, de, ef, fg, gh);
+
+I Function(A) flow8<A, B, C, D, E, F, G, H, I>(
+        B Function(A) ab,
+        C Function(B) bc,
+        D Function(C) cd,
+        E Function(D) de,
+        F Function(E) ef,
+        G Function(F) fg,
+        H Function(G) gh,
+        I Function(H) ih) =>
+    (A a) => pipe9(a, ab, bc, cd, de, ef, fg, gh, ih);
+
+J Function(A) flow9<A, B, C, D, E, F, G, H, I, J>(
+        B Function(A) ab,
+        C Function(B) bc,
+        D Function(C) cd,
+        E Function(D) de,
+        F Function(E) ef,
+        G Function(F) fg,
+        H Function(G) gh,
+        I Function(H) ih,
+        J Function(I) ji) =>
+    (A a) => pipe10(a, ab, bc, cd, de, ef, fg, gh, ih, ji);
--- test/function_test.dart
+++ test/function_test.dart
@@ -27,4 +27,107 @@ void main() {
       expect(identity(list), list); // Same reference
     });
   });
+
+  group('pipes', () {
+    int addOne(int n) {
+      return n + 1;
+    }
+
+    test('pipe returns its input unchanged', () {
+      expect(pipe(1), equals(1));
+    });
+
+    test('pipe2 should equal 2', () {
+      expect(pipe2(1, addOne), equals(2));
+    });
+
+    test('pipe3 should equal 3', () {
+      expect(pipe3(1, addOne, addOne), equals(3));
+    });
+
+    test('pipe4 should equal 4', () {
+      expect(pipe4(1, addOne, addOne, addOne), equals(4));
+    });
+
+    test('pipe5 should equal 5', () {
+      expect(pipe5(1, addOne, addOne, addOne, addOne), equals(5));
+    });
+
+    test('pipe6 should equal 6', () {
+      expect(pipe6(1, addOne, addOne, addOne, addOne, addOne), equals(6));
+    });
+
+    test('pipe7 should equal 7', () {
+      expect(
+          pipe7(1, addOne, addOne, addOne, addOne, addOne, addOne), equals(7));
+    });
+
+    test('pipe8 should equal 8', () {
+      expect(pipe8(1, addOne, addOne, addOne, addOne, addOne, addOne, addOne),
+          equals(8));
+    });
+
+    test('pipe9 should equal 9', () {
+      expect(
+          pipe9(1, addOne, addOne, addOne, addOne, addOne, addOne, addOne,
+              addOne),
+          equals(9));
+    });
+
+    test('pipe10 should equal 10', () {
+      expect(
+          pipe10(1, addOne, addOne, addOne, addOne, addOne, addOne, addOne,
+              addOne, addOne),
+          equals(10));
+    });
+  });
+
+  group('flows', () {
+    int addOne(int n) {
+      return n + 1;
+    }
+
+    test('should return 2', () {
+      expect(flow(addOne)(1), equals(2));
+    });
+
+    test('should return 3', () {
+      expect(flow2(addOne, addOne)(1), equals(3));
+    });
+    test('should return 4', () {
+      expect(flow3(addOne, addOne, addOne)(1), equals(4));
+    });
+
+    test('should return 5', () {
+      expect(flow4(addOne, addOne, addOne, addOne)(1), equals(5));
+    });
+
+    test('should return 6', () {
+      expect(flow5(addOne, addOne, addOne, addOne, addOne)(1), equals(6));
+    });
+
+    test('should return 7', () {
+      expect(
+          flow6(addOne, addOne, addOne, addOne, addOne, addOne)(1), equals(7));
+    });
+
+    test('should return 8', () {
+      expect(flow7(addOne, addOne, addOne, addOne, addOne, addOne, addOne)(1),
+          equals(8));
+    });
+
+    test('should return 9', () {
+      expect(
+          flow8(addOne, addOne, addOne, addOne, addOne, addOne, addOne, addOne)(
+              1),
+          equals(9));
+    });
+
+    test('should return 10', () {
+      expect(
+          flow9(addOne, addOne, addOne, addOne, addOne, addOne, addOne, addOne,
+              addOne)(1),
+          equals(10));
+    });
+  });
 }
